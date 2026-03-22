2b6af2861541299527a7b7415c6b50281f2c9e0f
Jit Changdar <jitchangdar7@gamil.com>
2026-03-22 19:52:34 +0530

    add authentic end_point

--- index.js
+++ index.js
@@ -1,4 +1,140 @@
-const userinfo=[];
-const organization=[];
-const bords=[];
-const issue=[];
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname)));
+const port = 4000;
+let user_id = 0;
+const Secrate_key = "jitchangdar@#4352323my-app/zetllle#$"
+const userinfo = [];
+const organization = [];
+const bords = [];
+const issue = [];
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "signup.html"));
+
+})
+app.get("/organization-create", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "organization.html"))
+})
+
+app.get("/board", (req, res) => {
+})
+
+app.post('/signup_post', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    const check_user = userinfo.find(item => item.email === email);
+    if (check_user) {
+        res.status(400).json({
+            message: "User already exists",
+        })
+    }
+
+
+    const user_name_token = jwt.sign(
+        { email },
+        Secrate_key
+    )
+
+    user_id++;
+    userinfo.push({ user_id, username, password, email });
+
+    res.json({
+        message: "User is Create Successfully",
+        token: user_name_token
+    })
+
+})
+
+
+app.post('/get-orgization-username', (req, res) => {
+    const token_name = req.headers.token;
+
+    if (!token_name) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decode = jwt.verify(token_name, Secrate_key);
+
+        const user = userinfo.find(
+            item => item.email === decode.email
+        );
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        res.json({ username: user.username });
+
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+});
+
+
+
+app.post("/add-create-create-org", (req, res) => {
+
+    const org_name = req.body.orgname;
+    const decription = req.body.description;
+    const token = req.body.token;
+
+    try {
+        const find_user = jwt.verify(token, Secrate_key);
+
+        if (!find_user) {
+            res.status(401).json({
+                message: "you are not valid user"
+            })
+        }
+
+        const verify_id = userinfo.find(item => item.email == find_user.email);
+
+        const user_id = verify_id.user_id;
+
+        organization.push({ user_id, org_name, decription });
+
+    }
+    catch (err) {
+        return res.status(401).json({
+            message: "invalid or expired token"
+        });
+    }
+
+
+})
+
+
+app.post("/signin", (req, res) => {
+
+})
+
+
+
+
+app.post("/add-create-member", (req, res) => {
+
+})
+
+
+app.post("/issue", (req, res) => {
+
+})
+
+
+
+app.post("/delete", (req, res) => {
+
+})
+
+app.listen(port, () => {
+    console.log("server is running");
+})
